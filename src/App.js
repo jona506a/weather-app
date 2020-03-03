7e54e03f8ae97b2f210a59931500dc9355babc56
jona506a <jobu@live.dk>
2020-03-03 13:27:13 +0100

    app changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Titles from "./components/Title";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "6d4a8e50c5982fd7e008b1702ff5bc69";
+// full api link: https://api.openweathermap.org/data/2.5/
+// weather?q=Copenhagen,dk&appid=6d4a8e50c5982fd7e008b1702ff5bc69
 
 class App extends React.Component {
     getWeather = async (e) => {
         e.preventDefault();
-        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/
-        weather?q=London,uk&appid=${API_KEY}`);
+        const api_call = await fetch(`https://api.openweathermap.org/
+        data/2.5/weather?q=Copenhagen,dk&appid=6d4a8e50c5982fd7e008b1702ff5bc69`);
         const data = await api_call.json();
         console.log(data);
     }
